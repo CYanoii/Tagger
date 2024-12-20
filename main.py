62d305aec82155f81a4c6d7bcf81aece7598c7b6
CYanoii <CYHei_mu@163.com>
2024-12-20 19:55:00 +0800

    将draw2DList函数转移到了tools.py中，添加了.gitignore，清理了之前误添加到版本库中的文件。

--- main.py
+++ main.py
@@ -1,73 +1,7 @@
 import copy
 
 from tagger import Tagger
-
-# 在传值被修改了的事情上有问题
-def draw(data, header = [], add_id = False):
-    # 用于将数据绘制好看。输入是严格的长度相等的二维list
-    # header: 表头
-    # add_id: 添加序号
-    
-    def chinese_len(s):
-        # 默认的len()会将中文字符长度视为1，但我们希望将其长度视为2
-
-        length = 0
-        for char in s:
-            # 判断字符是否是中文字符
-            if '\u4e00' <= char <= '\u9fff':  # 基本的汉字范围
-                length += 2
-            else:
-                length += 1
-        return length
-
-
-    data = copy.deepcopy(data)
-    item_len = len(data[0])
-
-    header_len = len(header)
-    if header_len != 0:
-        if header_len > item_len:
-            header = header[:item_len]
-        elif header_len < item_len:
-            header = header + [''] * (item_len - header_len)
-        data.insert(0, header)
-    
-    if add_id == True:
-        if len(header) != 0: id = -1
-        else: id = 0
-        for item in data:
-            id += 1
-            item.insert(0, str(id))
-
-    item_len = len(data[0])
-
-    item_max = [0] * item_len
-    for item in data:
-        for i in range(item_len):
-            item_max[i] = max(item_max[i], chinese_len(item[i]))
-
-    for item in data:
-        for i in range(item_len):
-                le = len(item[i])
-                cle = chinese_len(item[i])
-                if le == cle:    # 没有中文字符
-                    item[i] = item[i].center(item_max[i] + 2)
-                else:                                       # 有中文字符
-                    item[i] = item[i].center(item_max[i] + 2 - (cle - le))
-
-    # 绘制数据之间的表格行
-    str_line = '+'
-    for i in range(item_len): str_line += ('-' * (item_max[i] + 2) + '+')
-    
-    # 输出绘制图
-    print(str_line)
-    for item in data:
-        print(end='|')
-        for i in range(item_len):
-            print(item[i], end='|')
-        print()
-
-        print(str_line)
+import tools
 
 
 
@@ -79,14 +13,11 @@ if __name__ == '__main__':
     # tagger.projectImport('testdata/456.txt')
     # tagger.projectImport('testdata')
 
-    # data = [['我的世界', '22', '3'],['44', '555555', '6']]
-    # draw(data)
+    table = tagger.getProjectsTableByTags(["a96a87c8-3606-4a39-9684-81a0e2d8954e"])
+    tools.draw2DList(table, header=['项目名称', 'Uuid', '标签'], add_id=True)
 
-    # table = tagger.getProjectsTableByTags(["a96a87c8-3606-4a39-9684-81a0e2d8954e"])
-    # draw(table, header=['项目名称', 'Uuid', '标签'], add_id=True)
-
-    # table = tagger.getAllTagsTable()
-    # draw(table, header=['标签名称', 'Uuid'], add_id=True)
+    table = tagger.getAllTagsTable()
+    tools.draw2DList(table, header=['标签名称', 'Uuid'], add_id=True)
 
     # print(tagger.getUuidByTagName('我的世界'))
 
--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ import os
 import uuid
 import shutil
 import json
+import copy
 
 import errors
 
@@ -210,6 +211,73 @@ def openFileOrDirectory(path):
         raise errors.PathNotExistError(f"路径 '{path}' 不存在。")
     
     os.startfile(path)
+
+def draw2DList(data, header = [], add_id = False):
+    # 在传值被修改了的事情上有问题
+    # 用于将数据绘制好看。输入是严格的长度相等的二维list
+    # header: 表头
+    # add_id: 添加序号
+    
+    def chinese_len(s):
+        # 默认的len()会将中文字符长度视为1，但我们希望将其长度视为2
+
+        length = 0
+        for char in s:
+            # 判断字符是否是中文字符
+            if '\u4e00' <= char <= '\u9fff':  # 基本的汉字范围
+                length += 2
+            else:
+                length += 1
+        return length
+
+
+    data = copy.deepcopy(data)
+    item_len = len(data[0])
+
+    header_len = len(header)
+    if header_len != 0:
+        if header_len > item_len:
+            header = header[:item_len]
+        elif header_len < item_len:
+            header = header + [''] * (item_len - header_len)
+        data.insert(0, header)
+    
+    if add_id == True:
+        if len(header) != 0: id = -1
+        else: id = 0
+        for item in data:
+            id += 1
+            item.insert(0, str(id))
+
+    item_len = len(data[0])
+
+    item_max = [0] * item_len
+    for item in data:
+        for i in range(item_len):
+            item_max[i] = max(item_max[i], chinese_len(item[i]))
+
+    for item in data:
+        for i in range(item_len):
+                le = len(item[i])
+                cle = chinese_len(item[i])
+                if le == cle:    # 没有中文字符
+                    item[i] = item[i].center(item_max[i] + 2)
+                else:                                       # 有中文字符
+                    item[i] = item[i].center(item_max[i] + 2 - (cle - le))
+
+    # 绘制数据之间的表格行
+    str_line = '+'
+    for i in range(item_len): str_line += ('-' * (item_max[i] + 2) + '+')
+    
+    # 输出绘制图
+    print(str_line)
+    for item in data:
+        print(end='|')
+        for i in range(item_len):
+            print(item[i], end='|')
+        print()
+
+        print(str_line)
 
 
 if __name__ == '__main__':
